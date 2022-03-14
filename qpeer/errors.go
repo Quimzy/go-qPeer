b785930764045650126db0a4ae36f36f16206c5d
Quimzy <bam.boy2020@gmail.com>
2022-03-14 23:22:55 +0300

    Errors added

---
+++ qpeer/errors.go
@@ -0,0 +1,24 @@
+
+package qpeer
+
+type GreetError struct{}
+
+type VerifyError struct{}
+
+type PingError struct{}
+
+type InitError struct{}
+
+type IdError struct{}
+
+type RsaError struct{}
+
+type AesError struct{}
+
+type PeersError struct{}
+
+type LpeerError struct{}
+
+type PubkeyError struct{}
+
+type ByeError struct{}
