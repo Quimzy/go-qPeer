33d39bad2cc7cfe190ba30c2f51807fd1b6509b3
Quimzy <bam.boy2020@gmail.com>
2022-03-23 22:49:36 +0300

    Testing added

---
+++ qpeer/utils_test.go
@@ -0,0 +1,28 @@
+
+package qpeer
+
+import "testing"
+
+func TestSha1(t *testing.T){
+	msg := "quirk"
+	hash_msg_wanted := "f47c844721fa50459f2d6558d1904a688bc13ee2"
+	hash_msg_recvd := Sha1_encrypt(msg)
+
+	if hash_msg_recvd != hash_msg_wanted{
+		t.Errorf("Sha1 error. Wanted: %s, Recvd: %s", hash_msg_wanted, hash_msg_recvd )
+	}
+}
+
+func TestMd5(t *testing.T){
+	msg := "quirk"
+	hash_msg_wanted := "4342ba80a22071aab0e031922e0671d0"
+	hash_msg_recvd := Md5_encrypt(msg)
+
+	if hash_msg_recvd != hash_msg_wanted{
+		t.Errorf("Md5 error. Wanted: %s, Recvd: %s", hash_msg_wanted, hash_msg_recvd )
+	}
+}
+
+/*func TestIndex(t *testing.T){
+}*/
+
