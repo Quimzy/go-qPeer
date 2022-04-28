dfc257d23d9d4545a360a9412c08cb600c59d593
Quimzy <bam.boy2020@gmail.com>
2022-04-28 21:05:44 +0300

    IsUDPOnline done

--- qpeer/proto.go
+++ qpeer/proto.go
@@ -4,7 +4,24 @@ package lib
 import (
 	"github.com/quirkio/Endpoint"
 	"net"
-	"os"
+	"fmt"
+	"time"
 )
 
-func is_online(peerip string, port string) bool{}
+func IsUDPOnline(peerip string, port string) bool{
+	timeout := 5 * time.Second
+	addr := fmt.Sprintf("%s:%s", host, port)
+	
+	conn, err := net.DialTimeout("udp", addr, timeout)
+	if err != nil{
+		return false
+	}
+
+	if conn != nil{
+		conn.Close()
+		return true
+	}
+
+	return false
+}
+
