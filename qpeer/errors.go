042fe95a72496a051d918a3c420432dc5e013e3a
Quimzy <bam.boy2020@gmail.com>
2022-07-05 18:36:37 +0300

    error handling: function moved to qpeer/errors.go

--- qpeer/errors.go
+++ qpeer/errors.go
@@ -1,6 +1,10 @@
 package lib
 
-import "errors"
+import (
+	"errors"
+	"log"
+	"os"
+)
 
 //RSA errors
 var ErrorRSA = errors.New("qpeer: can't encrypt/decrypt rsa") //handled
@@ -74,3 +78,34 @@ var ErrorKdecpeerinfo = errors.New("qpeer: can't decrypted AES encrypted peerinf
 var ErrorBye = errors.New("qpeer: peer did not send bye back")
 
 var ErrorPenckey = errors.New("qpeer: can't read/use RSA encrypted AES key")
+
+// ErrorHandling
+
+func openLogFile(path string) (*os.File, error) {
+	logFile, err := os.OpenFile(path, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
+	if err != nil {
+		return nil, err
+	}
+	return logFile, nil
+}
+
+func ErrorHandling(err error) {
+	errorFile, logfile_err := openLogFile("errors.log")
+	if logfile_err != nil {
+		log.Println("An error occured.. qPeer won't be able to log errors")
+		return
+	}
+
+	customLog := log.New(errorFile, "[ERROR] ", log.LstdFlags|log.Lshortfile)
+
+	if errors.Is(err, ErrorJSON) || errors.Is(err, ErrorRSA) || errors.Is(err, ErrorAES) { //normal errors logged to file
+		customLog.Println(err)
+		return
+	}
+
+	if errors.Is(err, ErrorReadLpeer) || errors.Is(err, ErrorWriteLpeer) || errors.Is(err, ErrorReadPeers) || errors.Is(err, ErrorWritePeers) || errors.Is(err, ErrorReadTempPeers) || errors.Is(err, ErrorWriteTempPeers) || errors.Is(err, ErrorReadRSA) || errors.Is(err, ErrorWriteRSA) || errors.Is(err, ErrorRSAPrivKey) || errors.Is(err, ErrorImportRSA) || errors.Is(err, ErrorExportRSA) { //critical error logged to file
+		customLog.Fatalln(err)
+	}
+
+	//if errors.Is(err, ) connection & verification delete peer from db
+}
--- qpeer/utils.go
+++ qpeer/utils.go
@@ -12,7 +12,6 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"encoding/pem"
-	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -788,35 +787,4 @@ func Save_temp_peers(enc_temp_peers string, privkey *rsa.PrivateKey, all_peers A
 	}
 
 	return nil
-}
-
-// ErrorHandling
-
-func openLogFile(path string) (*os.File, error) {
-	logFile, err := os.OpenFile(path, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
-	if err != nil {
-		return nil, err
-	}
-	return logFile, nil
-}
-
-func ErrorHandling(err error) {
-	errorFile, logfile_err := openLogFile("errors.log")
-	if logfile_err != nil {
-		log.Println("An error occured.. qPeer won't be able to log errors")
-		return
-	}
-
-	customLog := log.New(errorFile, "[ERROR] ", log.LstdFlags|log.Lshortfile)
-
-	if errors.Is(err, ErrorJSON) || errors.Is(err, ErrorRSA) || errors.Is(err, ErrorAES) { //normal errors logged to file
-		customLog.Println(err)
-		return
-	}
-
-	if errors.Is(err, ErrorReadLpeer) || errors.Is(err, ErrorWriteLpeer) || errors.Is(err, ErrorReadPeers) || errors.Is(err, ErrorWritePeers) || errors.Is(err, ErrorReadTempPeers) || errors.Is(err, ErrorWriteTempPeers) || errors.Is(err, ErrorReadRSA) || errors.Is(err, ErrorWriteRSA) || errors.Is(err, ErrorRSAPrivKey) || errors.Is(err, ErrorImportRSA) || errors.Is(err, ErrorExportRSA) { //critical error logged to file
-		customLog.Fatalln(err)
-	}
-
-	//if errors.Is(err, ) connection & verification delete peer from db
 }
